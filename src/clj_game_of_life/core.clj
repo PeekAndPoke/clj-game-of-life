650c7c1c2194aa142876c5d5dd4fd6c0887d0fc8
PeekAndPoke <karsten.john.gerber@googlemail.com>
2018-12-26 22:11:14 +0100

    cosmetics

--- src/clj_game_of_life/core.clj
+++ src/clj_game_of_life/core.clj
@@ -1,7 +1,9 @@
 (ns clj-game-of-life.core
   (:require [clj-game-of-life.gol :as gol]))
 
+;; Why do we have the minus here: "-main" ... saw it somewhere
 (defn -main []
   (println "Hello Game Of Life!")
+  ;; Run the GoL for 500 generations with 25 ms of sleep time in between
   (gol/run 500 25)
   (println "ByeBye Game Of Life!"))
