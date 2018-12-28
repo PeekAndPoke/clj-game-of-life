a188c6425149499f12a0f4968cf666f1417ef88b
PeekAndPoke <karsten.john.gerber@googlemail.com>
2018-12-28 07:15:22 +0100

    refactoring. better board creation. putting patterns on board

--- src/clj_game_of_life/core.clj
+++ src/clj_game_of_life/core.clj
@@ -1,9 +1,15 @@
 (ns clj-game-of-life.core
-  (:require [clj-game-of-life.gol :as gol]))
+  (:require [clj-game-of-life.gol :as gol]
+            [clj-game-of-life.patterns :as patterns]))
 
 ;; Why do we have the minus here: "-main" ... saw it somewhere
 (defn -main []
   (println "Hello Game Of Life!")
   ;; Run the GoL for 500 generations with 25 ms of sleep time in between
-  (gol/run 20 25)
+  (gol/run
+    (gol/put-patterns (gol/create-board 80 50) [[0 0 patterns/cosper-glider-gun]
+                                                [50 40 patterns/still-block]
+                                                [55 36 patterns/still-block]])
+    1500
+    15)
   (println "ByeBye Game Of Life!"))
--- src/clj_game_of_life/gol.clj
+++ src/clj_game_of_life/gol.clj
@@ -1,27 +1,7 @@
 (ns clj-game-of-life.gol
   (:require [clojure.string :as str]))
 
-;; lets have the board in custom dim
-(def dx 50)
-(def dy 50)
-
-;; lets use a compact generator function (fn [i]) that maps on a index seq [0 .. (dx * dy)]
-(def ^:private initial (into [] (map vec (partition dx 
-                                                    (into [] (let [ ;; approx the matrix center
-                                                                   dx' (quot dx 2)
-                                                                   dy' (quot dy 2)
-                                                                   ;; 
-                                                                   unos #{[(dec dy') dx']
-                                                                          [(dec dy') (inc dx')]
-                                                                          [dy' (dec dx')]
-                                                                          [dy' dx']
-                                                                          [(inc dy') dx']}] 
-                                                               (doall (map 
-                                                                       (fn [i]
-                                                                         (let [x (mod i dx)
-                                                                               y (quot i dy)]                                                
-                                                                           (if (contains? unos [y x]) 1 0)))
-                                                                       (range (* dx dy))))))))))
+(defn- to-vectors [input] (vec (map vec input)))
 
 (defn- is-alive? [field x y]
   (let [result (get (get field y) x)]
@@ -29,15 +9,11 @@
 
 (defn- count-alive-neighbors [field x y]
   ;; cme: using list comprehension like in algebra/haskel (e.g. for all [x,y] where x > 2*y) is done using :when condition
-  (apply + (map (fn [[dx dy]] (is-alive? field (+ x dx) (+ y dy))) (for [dx (range -1 2) dy (range -1 2) :when (or (not= dx 0) (not= dy 0))]
-    [dx dy]))
-    ))
+  (apply + (map (fn [[dx dy]] (is-alive? field (+ x dx) (+ y dy)))
+                (for [dx (range -1 2) dy (range -1 2) :when (or (not= dx 0) (not= dy 0))] [dx dy]))))
 
 (defn- lives-in-next-generation [field x y]
   ; Here we apply the default GoL rules
-  ;; using let we can memoize results for avoiding recalc
-  ;; (un)fortunatelly clojure is not a pure language like haskell so idempotenz does not work, so 
-  ;; recuring calculations are not cached by default
   (let [alive-neighbors (count-alive-neighbors field x y)]
     (or (= 3 alive-neighbors)
         (and (= 2 alive-neighbors) (= 1 (is-alive? field x y))))))
@@ -47,17 +23,13 @@
 
 (defn- next-step [field]
   ; Make sure we have a vector, as we need random access to the elements. This is not possible with lists.
-  (vec
+  (to-vectors
+    ; Iterate the rows
     (map-indexed
-      ; Iterate the rows
       (fn [idr row]
-        ; Same deal. We need vectors for random access.
-        (vec
-          ; Iterate the columns and calculate the next generation of every cell
-          (map-indexed (fn [idc _] (next-generation field idc idr)) row))
-        )
-      field
-      )))
+        ; Iterate the columns and calculate the next generation of every cell
+        (map-indexed (fn [idc _] (next-generation field idc idr)) row))
+      field)))
 
 (defn- line-to-string [line]
   (apply str (map #(if (= 0 %) "." "o") line)))
@@ -65,25 +37,45 @@
 (defn- board-to-string [lines]
   (str/join "\n" (map line-to-string lines)))
 
-(def ^:private current (atom initial))
+(defn create-board [dx dy]
+  (to-vectors
+    (partition dx (repeat (* dx dy) 0))))
 
-(defn run [rounds sleepMs]
+(defn put-pattern [board x y pattern]
+  (to-vectors
+    (map-indexed
+      (fn [idr row]
+        (map-indexed
+          (fn [idc it]
+            (let [in-pattern (get (get pattern (- idr y)) (- idc x))]
+              (if (= 1 in-pattern) in-pattern it)))
+          row))
+      board)))
+
+(defn put-patterns [board xy-patterns]
+  (loop [rest-patterns xy-patterns result board]
+    (if (empty? rest-patterns)
+      result
+      (recur (rest rest-patterns) (apply put-pattern (cons result (first rest-patterns)))))))
+
+(defn run [initial-board rounds sleepMs]
   ; Some debug output ... to see if we are running
   (println "Let's go")
-  ; 'doall' forces evaluation of the lazy seq
-  (doall (for [x (range rounds)]
-           ; Using let to pre-calculate the printable output of the next step (to avoid flickering)
-           (let [output (board-to-string (deref current))]
-             ; Print 50 empty lines. Is there a better way?
-             (repeatedly 50 (println ""))
-             ; Print the # round
-             (println (str "Round " x))
-             ; Print the board
-             (println output)
-             ; Calculate the next step and set it on current. How does this work exactly?
-             (swap! current next-step)
-             ; Wait a moment
-             (Thread/sleep sleepMs))
-           ))
+  (let [current (atom initial-board)]
+    ; 'doall' forces evaluation of the lazy seq
+    (doall (for [x (range rounds)]
+             ; Using let to pre-calculate the printable output of the next step (to avoid flickering)
+             (let [output (board-to-string (deref current))]
+               ; Print 50 empty lines. Is there a better way?
+               (println (apply str (repeat 50 "\n")))
+               ; Print the # round
+               (println (str "Round " x))
+               ; Print the board
+               (println output)
+               ; Calculate the next step and set it on current. How does this work exactly?
+               (swap! current next-step)
+               ; Wait a moment
+               (Thread/sleep sleepMs))
+             )))
   ; Some debug output ... to see if we are done
   (println "Done"))
